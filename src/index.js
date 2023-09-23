cca0a5a18548b0b6e4c00df2fb7915d4dabbc75d
filmbronte <122818201+filmbronte@users.noreply.github.com>
2023-09-23 18:30:30 +0300

    add public folder and set up some views

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.static(path.resolve(__dirname,'public')))
 
 app.engine('hbs', handlebars.engine({
 	extname: 'hbs',
@@ -12,6 +15,12 @@ app.set('views', 'src/views')
 
 app.get('/', (req, res) => {
 	res.render('index');
+});
+app.get('/create', (req, res) => {
+	res.render('create');
+});
+app.get('/about', (req, res) => {
+	res.render('about');
 });
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
