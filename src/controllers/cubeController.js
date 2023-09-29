6636fa2f7d957067fcc15559d20c9b9ce63b0126
filmbronte <122818201+filmbronte@users.noreply.github.com>
2023-09-29 17:01:05 +0300

    add accessory page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,28 +1,36 @@
 const router = require('express').Router();
+
 const cubeManager = require('../managers/cubeManager');
+const accessoryManager = require('../managers/accessoryManager');
 
 router.get('/create', (req, res) => {
 	res.render('create')
 })
 router.get('/:cubeId/details', async (req, res) => {
-	const cube =  await cubeManager.getOne(req.params.cubeId).lean();
+	const cube = await cubeManager.getOne(req.params.cubeId).lean();
 
 	if (!cube) {
 		return res.redirect('/notfound')
 	}
-	res.render('details', {...cube});
+	res.render('details', { ...cube });
 })
 
 router.post('/create', async (req, res) => {
-	const {name, description, imageUrl, difficultyLevel} = req.body;
+	const { name, description, imageUrl, difficultyLevel } = req.body;
 
 	cubeManager.create({
-		name, 
-		description, 
-		imageUrl, 
+		name,
+		description,
+		imageUrl,
 		difficultyLevel: Number(difficultyLevel)
 	});
 	res.redirect('/');
+})
+
+router.get('/:cubeId/attach-accessory', async (req, res) => {
+	const cube = await cubeManager.getOne(req.params.cubeId).lean();
+
+	res.render('accessory/attach', { cube });
 })
 
 module.exports = router;
