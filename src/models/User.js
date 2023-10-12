0b6cb5608d98dfccbfacc79a2a24a16f8f273a56
filmbronte <122818201+filmbronte@users.noreply.github.com>
2023-10-12 13:09:50 +0300

    use hashed password

--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-    username: String,
-    password: {
+	username: String,
+	password: {
 		type: String,
 		// validate: {
 		// 	validator: function(value) {
@@ -18,6 +19,12 @@ userSchema.virtual('repeatPassword')
 		if (value !== this.password) {
 			throw new mongoose.MongooseError('Passwords do not match!');
 		}
+	})
+
+userSchema.pre('save', async function() {
+	const hash = await bcrypt.hash(this.password, 10);
+
+	this.password = hash;
 })
 
 const User = mongoose.model('User', userSchema);
