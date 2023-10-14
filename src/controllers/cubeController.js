fb75e2e196dd374b725c6b50b0454ad96901842c
filmbronte <122818201+filmbronte@users.noreply.github.com>
2023-10-14 16:26:12 +0300

    fix dropdown menu

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -3,6 +3,8 @@ const router = require('express').Router();
 const cubeManager = require('../managers/cubeManager');
 const accessoryManager = require('../managers/accessoryManager');
 
+const getDifficultyOptionsViewData = require('../utils/viewHelpers');
+
 router.get('/create', (req, res) => {
 	res.render('cube/create')
 })
@@ -50,7 +52,9 @@ router.post('/:cubeId/attach-accessory', async (req, res) => {
 router.get('/:cubeId/delete', async (req, res) => {
 	const cube = await cubeManager.getOne(req.params.cubeId).lean();
 
-	res.render('cube/delete', { cube });
+	const options = getDifficultyOptionsViewData(cube.difficultyLevel);
+
+	res.render('cube/delete', { cube, options });
 })
 
 router.post('/:cubeId/delete', async (req, res) => {
@@ -59,10 +63,13 @@ router.post('/:cubeId/delete', async (req, res) => {
 	res.redirect('/')
 })
 
+
 router.get('/:cubeId/edit', async (req, res) => {
 	const cube = await cubeManager.getOne(req.params.cubeId).lean();
 
-	res.render('cube/edit', { cube })
+	const options = getDifficultyOptionsViewData(cube.difficultyLevel);
+	
+	res.render('cube/edit', { cube, options })
 })
 
 router.post('/:cubeId/edit', async (req, res) => {
---
+++ src/utils/viewHelpers.js
@@ -0,0 +1,20 @@
+function getDifficultyOptionsViewData(difficultyLevel) {
+	const titles = [
+		'Very Easy',
+		'Easy',
+		'Medium (Standart 3x3)',
+		'Intermediate',
+		'Expert',
+		'Hardcore',
+	]
+
+	const options = titles.map((title, index) => ({
+		title: `${index + 1} - ${title}`,
+		value: index + 1,
+		selected: Number(difficultyLevel) === index + 1,
+	}))
+
+	return options;
+}
+
+module.exports = getDifficultyOptionsViewData;
